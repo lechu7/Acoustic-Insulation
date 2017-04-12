2055f55d73a5c87fc4088a8dfa3b090232d22597
danieln96 <niczyporuk96@interia.pl>
2017-04-13 01:47:38 +0200

    outliners

--- src/main/java/pl/io/Statistics.java
+++ src/main/java/pl/io/Statistics.java
@@ -19,7 +19,28 @@ public class Statistics {
 		return list;
 	}
 
-	public List outliners(Object aList_from_file) {
-		throw new UnsupportedOperationException();
+	public static  List<Double> outliners(List<Double> list) 
+	{
+		//obliczenie sredniej
+		double average=0;
+		for(int i=0;i<list.size();i++)
+			average+=list.get(i);
+		average=average/list.size();
+		//obliczenie odchylenia standardowego
+		double deviation=0;
+		for(int i=0;i<list.size();i++)
+			deviation += Math.pow(list.get(i)-average, 2);
+		deviation = deviation/(list.size()-1);
+		deviation = Math.sqrt(deviation);
+		//obliczenie wasow
+		double upper = average+deviation;
+		double bottom = average-deviation;
+		// sprawdzenie i zamiana
+		for(int i=0;i<list.size();i++)
+		{
+			if(list.get(i)>upper) list.set(i, upper);
+			if(list.get(i)<bottom) list.set(i, bottom);
+		}
+		return list;
 	}
 }
