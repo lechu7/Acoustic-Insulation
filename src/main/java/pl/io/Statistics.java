48c37d4c2fd976a544411bf03338967f15985a4b
danieln96 <niczyporuk96@interia.pl>
2017-04-13 01:45:58 +0200

    statistics class

--- src/main/java/pl/io/Statistics.java
+++ src/main/java/pl/io/Statistics.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public class Statistics {
 
-	public double normalization(Object aList_without_outliners) {
-		throw new UnsupportedOperationException();
+	public static List<Double> normalization(List<Double> list) 
+	{
+		double max = list.get(0);
+		double min = list.get(0);
+		//Obliczenie wartosci najmniejszej i najwiekszej
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i)>max) max = list.get(i);
+			if (list.get(i)<min) min = list.get(i);
+		}
+		//normalizacja
+		for(int i=0;i<list.size();i++)
+			list.set(i, (list.get(i)-min)/(max-min)*2-1); 
+		return list;
 	}
 
 	public List outliners(Object aList_from_file) {
