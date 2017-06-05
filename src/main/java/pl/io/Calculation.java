f082adfbc2f477634cd46bed2744ba623d12b6ac
Mateusz Tarnowski <mac2796@gmail.com>
2017-06-05 22:32:29 +0200

    Fixed isolation calculation in Calculation class

--- src/main/java/pl/io/Calculation.java
+++ src/main/java/pl/io/Calculation.java
@@ -3,14 +3,11 @@ package pl.io;
 public class Calculation {
 
 	public static double[] calculateIsolation(double[] firstChannel, double[] secondChannel, double frequency){
-		Complex[] fft1 = transformation(firstChannel);
-		Complex[] fft2 = transformation(secondChannel);
-		double[] energySpectrum1 = PreparingFFT.EnergySpectrum(fft1, firstChannel.length);
-		double[] energySpectrum2 = PreparingFFT.EnergySpectrum(fft2, secondChannel.length);
-		double[] energySpectrumDiff = diff(energySpectrum1, energySpectrum2);
-		double[] dBs = dbs(energySpectrumDiff, frequency, 1, fft1.length);
+		double[] dbs1 = calcDBs(firstChannel, frequency);
+		double[] dbs2 = calcDBs(secondChannel, frequency);
+		double[] difference = diff(dbs1, dbs2);
 		
-		return dBs;		
+		return difference;
 	}
 	
 	public static double[] calcDBs(double[] channel, double frequency){
@@ -20,25 +17,25 @@ public class Calculation {
 		return dBs;
 	}
 	
-	public static Complex[] transformation(double[] samples) {
+	private static Complex[] transformation(double[] samples) {
     	Complex[] fft = FFT.fft(PreparingFFT.DataPreparingForFFT(samples));
     	return fft;
 	}
 
-	public static double[] diff(double[] listch1, double[] listch2) {
+	private static double[] diff(double[] listch1, double[] listch2) {
 		double[] listdiff = new double[listch1.length];
 		for(int i = 0; i < listch1.length; i++)
 			listdiff[i] = listch1[i] - listch2[i];
 		return listdiff;
 	}
 	
-	public static double[] dbs(double[] PowerS, double fs, double P0, int N){
+	private static double[] dbs(double[] PowerS, double fs, double P0, int N){
         int i;
         
         double f;
         double dB;
         
-        double[] result = new double[(int)fs];
+        double[] result = new double[(int)fs/2];
         int a = 0;
         double sum = 0;
         int freq = 0;
