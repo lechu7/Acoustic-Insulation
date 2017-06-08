d14b332286aa932908ef59b5500e051294b4d538
lechu7 <lechu0@vp.pl>
2017-06-08 20:38:45 +0200

    readCSV delete

--- src/main/java/pl/io/IO.java
+++ src/main/java/pl/io/IO.java
@@ -21,29 +21,5 @@ public class IO
 	      pw.close();
 	  }
 
-	public static List<Double[]> readCSV(String path) throws FileNotFoundException{
-	      File file = new File(path);
-	      Scanner s = new Scanner(file);
-	      List<Double[]> list = new ArrayList<Double[]>();
-	      
-	      s.nextLine();
-	      
-	      while(s.hasNextLine()){
-	      String line = s.nextLine();
-	      line = line.replace(',', '.');
-	      String[] first = line.split(";");
-	      
-	      Double[] second = new Double[]{
-	    		  Double.parseDouble(first[0]), //frequency value [Hz]
-	    		  Double.parseDouble(first[1]), //sound pressure from 1st channel [dB]
-	    		  Double.parseDouble(first[2]),	//sound pressure from 2nd channel [dB]
-	    		  Double.parseDouble(first[3]),	//difference value [dB]
-	    		  };
-	      
-	      list.add(second);
-	      }
-	      
-	      s.close();
-	      return list;
-	  }
+
 }
