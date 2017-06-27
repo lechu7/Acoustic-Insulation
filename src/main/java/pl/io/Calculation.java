fee90eb050ee9e57dec19edf141259d811c370a3
Mateusz Tarnowski <mac2796@gmail.com>
2017-06-27 11:13:39 +0200

    Documentation for Calculation class

--- src/main/java/pl/io/Calculation.java
+++ src/main/java/pl/io/Calculation.java
@@ -1,10 +1,20 @@
 package pl.io;
 
+/**
+ * Class used for calculating transformation, isolation and difference in sound.
+ */
 public class Calculation {
 	private static double[] calibrationChannel0, calibrationChannel1;
 	private static boolean firstChannelIsBeforeBarrier = true;
 	private static double minimalSignalStrength = 0;
 	
+	/**
+	 * Calculates isolation in dBs for two specified channels.
+	 * @param firstChannel	Sampled sound from first microphone
+	 * @param secondChannel Sampled sound from second microphone
+	 * @param frequency		Sampling frequency
+	 * @return				Calculated sound isolation
+	 */
 	public static double[] calculateIsolation(double[] firstChannel, double[] secondChannel, double frequency){
 		double[] dbs1 = calcDBs(firstChannel, frequency);
 		double[] dbs2 = calcDBs(secondChannel, frequency);
@@ -24,6 +34,11 @@ public class Calculation {
 		return difference;
 	}
 	
+	/**
+	 * Calculates sound volume.
+	 * @param channel	Sampled sound
+	 * @param frequency	Sampling frequency
+	 */
 	public static double[] calcDBs(double[] channel, double frequency){
 		Complex[] fft = transformation(channel);
 		double[] energySpectrum = PreparingFFT.EnergySpectrum(fft, channel.length);
@@ -35,7 +50,11 @@ public class Calculation {
     	Complex[] fft = FFT.fft(PreparingFFT.DataPreparingForFFT(samples));
     	return fft;
 	}
-
+	/**
+	 * Calculates difference of two arrays
+	 * @param listch1	Minuend array
+	 * @param listch2	Subtrahend array
+	 */
 	public static double[] diff(double[] listch1, double[] listch2) {
 		double[] listdiff = new double[listch1.length];
 		for(int i = 0; i < listch1.length; i++)
@@ -72,28 +91,52 @@ public class Calculation {
         return result;
     }
 	
+	/**
+	 * Sets calibration for channel0
+	 * @param cal Calibration array
+	 */
 	public static void setCalibrationChannel0(double[] cal){
 		calibrationChannel0 = cal;
 	}
 	
+	/**
+	 * Sets calibration for channel1
+	 * @param cal Calbration array
+	 */
 	public static void setCalibrationChannel1(double[] cal){
 		calibrationChannel1 = cal;
 	}
 	
+	/**
+	 * Setter method used to set which channel is placed before the barrier.
+	 * @param b
+	 */
 	public static void setFirstChannelIsBeforeBarrier(boolean b){
 		if (firstChannelIsBeforeBarrier != b){
 			firstChannelIsBeforeBarrier = b;
 		}
 	}
 	
+	/**
+	 * @return true - when first channel is placed before the barrier.
+	 */
 	public static boolean isFirstChannelBeforeBarrier(){
 		return firstChannelIsBeforeBarrier;
 	}
 	
+	/**
+	 * Sets minimal required signal strength.
+	 * @param min Value in percentages
+	 */
 	public static void setMinimalSignalStrength(double min){
 		minimalSignalStrength = 32768 * (min / 100);
 	}
 	
+	/**
+	 * Checks if sampled signal passed as argument is considered strong enough.
+	 * @param channel Sampled signal
+	 * @return true - when signal is strong enough
+	 */
 	public static boolean isSignalStrongEnough(double[] channel){
 		for (int i = 0; i < channel.length; i++){
 			if (Math.abs(channel[i]) >= minimalSignalStrength) return true;
